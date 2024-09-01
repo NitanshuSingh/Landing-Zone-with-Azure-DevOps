a43a38b4842347833d15e57ce1955459163a4e67
nitanshu <nitanshusingh55@gmail.com>
2024-09-01 19:57:18 +0530

    emptying backend

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
-# terraform {
-#   backend "azurerm" {
-#     resource_group_name  = "dev-rg"
-#     storage_account_name = "devstrg23"
-#     container_name       = "dev-container"
-#     key                  = "dev.tfstate"
-#   }
-# }
+terraform {
+  backend "azurerm" {
+    resource_group_name  = ""
+    storage_account_name = ""
+    container_name       = ""
+    key                  = ""
+  }
+}
